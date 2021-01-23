58a8d6b41028e1465dfa81edadd24e0e657a9c01
Samuele Negrini <samuele.negrini@mail.polimi.it>
2021-01-23 17:58:57 +0100

    Fix ticket integration test

--- Code/Server/CLupEJB/src/test/java/it/polimi/se2/clup/CLupEJB/integration/TicketIntegrationTest.java
+++ Code/Server/CLupEJB/src/test/java/it/polimi/se2/clup/CLupEJB/integration/TicketIntegrationTest.java
@@ -2,28 +2,37 @@ package it.polimi.se2.clup.CLupEJB.integration;
 
 import it.polimi.se2.clup.CLupEJB.entities.TicketEntity;
 import it.polimi.se2.clup.CLupEJB.services.TicketService;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TicketIntegrationTest {
-    private static final int TICKET_ID = 1;
     private static final String PASS_CODE = "test_pass_code";
     private static final String INVALID_PASS_CODE = "test_pss_cd";
 
-    private EntityManagerFactory emf;
-    private EntityManager em;
+    private static int LAST_INSERT_ID = 0;
+
+    private static EntityManagerFactory emf;
+    private static EntityManager em;
+
+    @BeforeAll
+    public static void setUpBeforeClass() {
+        emf = Persistence.createEntityManagerFactory("CLupEJB-testing");
+    }
+
+    @AfterAll
+    public static void tearDownAfterClass() {
+        if (emf != null) {
+            emf.close();
+        }
+    }
 
     @BeforeEach
     void setUp() {
-        emf = Persistence.createEntityManagerFactory("CLupEJB-testing");
         em = emf.createEntityManager();
         createTestData();
     }
@@ -34,24 +43,22 @@ public class TicketIntegrationTest {
             removeTestData();
             em.close();
         }
-        if (emf != null) {
-            emf.close();
-        }
     }
 
     private void createTestData() {
         TicketEntity ticket = new TicketEntity();
-        ticket.setTicketId(TICKET_ID);
         ticket.setPassCode(PASS_CODE);
 
         em.getTransaction().begin();
         em.persist(ticket);
+        em.flush();
+        LAST_INSERT_ID = ticket.getTicketId();
         em.getTransaction().commit();
     }
 
     private void removeTestData() {
         em.getTransaction().begin();
-        TicketEntity ticket = em.find(TicketEntity.class, TICKET_ID);
+        TicketEntity ticket = em.find(TicketEntity.class, LAST_INSERT_ID);
         if (ticket != null) {
             em.remove(ticket);
         }
@@ -61,9 +68,16 @@ public class TicketIntegrationTest {
     @Test
     public void testValidGeneratedValueTicket_ValidPassCode() {
         TicketService ticketService = new TicketService(em);
-        TicketEntity ticket = ticketService.findTicketById(TICKET_ID);
+        TicketEntity ticket = ticketService.findTicketById(LAST_INSERT_ID);
         assertNotNull(ticket);
         assertEquals(PASS_CODE, ticket.getPassCode());
+    }
+
+    @Test
+    public void testValidGeneratedValueTicket_InvalidPassCode() {
+        TicketService ticketService = new TicketService(em);
+        TicketEntity ticket = ticketService.findTicketById(555);
+        assertNull(ticket);
     }
 
 }
