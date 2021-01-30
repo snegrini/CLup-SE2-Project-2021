2eaffae967a8a79c947643718abdb85433356645
Samuele Negrini <samuele.negrini@mail.polimi.it>
2021-01-30 14:56:58 +0100

    Fix image servlets.

--- Code/Server/CLupWeb/src/main/java/it/polimi/se2/clup/CLupWeb/controllers/ImageServlet.java
+++ Code/Server/CLupWeb/src/main/java/it/polimi/se2/clup/CLupWeb/controllers/ImageServlet.java
@@ -14,11 +14,25 @@ import java.nio.file.Path;
 public class ImageServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String filename = request.getPathInfo().substring(1);
-        String uploadLocation = getServletContext().getInitParameter("upload.location");
+        // Checks URL is well formed.
+        String filename;
+        try {
+             filename = request.getPathInfo().substring(1);
+        } catch (NullPointerException e) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Could not find the requested image.");
+            return;
+        }
 
-        if (!Files.exists(Path.of(uploadLocation))) {
+        // Retrieves image name and check existence.
+        String uploadLocation = getServletContext().getInitParameter("upload.location");
+        Path fullPath = Path.of(uploadLocation + "/" + filename);
+
+        if (!Files.exists(Path.of(uploadLocation)) ) {
             response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Could not find the image folder. Please fix the specified folder in the web.xml file.");
+            return;
+        }
+        if (filename.isEmpty() || !Files.exists(fullPath)) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Could not find the requested image.");
             return;
         }
 
