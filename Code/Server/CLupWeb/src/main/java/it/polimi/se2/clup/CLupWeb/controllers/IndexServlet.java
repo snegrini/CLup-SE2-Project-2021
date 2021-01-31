97b7f01960edbf328bfae88a4ea398cdc8c017e4
Samuele Negrini <samuele.negrini@mail.polimi.it>
2021-02-01 00:25:18 +0100

    Add CSS

--- Code/Server/CLupWeb/src/main/java/it/polimi/se2/clup/CLupWeb/controllers/IndexServlet.java
+++ Code/Server/CLupWeb/src/main/java/it/polimi/se2/clup/CLupWeb/controllers/IndexServlet.java
@@ -1,6 +1,12 @@
 package it.polimi.se2.clup.CLupWeb.controllers;
 
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,11 +18,27 @@ import java.io.IOException;
 @WebServlet(name = "IndexServlet", value = "")
 public class IndexServlet extends HttpServlet {
 
+    private TemplateEngine templateEngine;
+
+    public void init() {
+        ServletContext servletContext = getServletContext();
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        this.templateEngine = new TemplateEngine();
+        this.templateEngine.setTemplateResolver(templateResolver);
+        templateResolver.setSuffix(".html");
+    }
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession(false);
         if (session == null || session.getAttribute("user") == null) { // No logged-in user found, so redirect to login page.
-            RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/index.html");
-            view.forward(request, response);
+            response.setContentType("text/html");
+
+            ServletContext servletContext = getServletContext();
+            WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+            String path = "/WEB-INF/index.html";
+
+            templateEngine.process(path, ctx, response.getWriter());
         } else {
             response.sendRedirect(request.getContextPath() + "/dashboard");
         }
